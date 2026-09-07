0aac4038999ba1553be226c467a0b3ff966ddd9f
NaimaMuse <Naciimaaahmed@gmail.com>
2026-09-07 11:15:10 +0300

    new files

--- course-05-react-basics/Module 3/my-react-app/my-app/src/Components/Home.jsx
+++ course-05-react-basics/Module 3/my-react-app/my-app/src/Components/Home.jsx
@@ -1,9 +1,11 @@
+import Camera from '../assets/images/camera.png';
 function Home() {
   return (
     <div className="home-page">
       <h1 className="home-title">This is the Home Page</h1>
       <p className="home-text">
         Welcome to my React website!
+      <img src={Camera} alt="Hero" />
       </p>
     </div>
   )
